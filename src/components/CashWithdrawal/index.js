183d23d7c912c6e23ac3fd315d3f4ad1ee5ef39e
Gangabhavani622 <115251057+Gangabhavani622@users.noreply.github.com>
2022-12-12 18:46:34 +0530

    Update index.js

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -15,21 +15,24 @@ class CashWithdrawal extends Component {
   render() {
     const {count} = this.state
     const {denominationsList} = this.props
+    const name="Sarah Williams"
+    const initialLetter=name.slice(0,1)
     // console.log(denominationsList)
 
     return (
       <div className="container">
         <div className="contact-container">
           <div className="name-container">
-            <h1 className="first-letter">S</h1>
-            <h1 className="name">Sarah Williams</h1>
+            <p className="first-letter">{initialLetter}</p>
+            <h1 className="name">{name}</h1>
           </div>
           <div className="balance-container">
             <p className="balance-label">Your Balance</p>
 
-            <p className="count">{count}</p>
+            <p className="count">{count} <br/> 
+              <span className="para">In Rupees</span>
+          </p>
           </div>
-          <p className="para">In Rupees</p>
           <p className="count">Withdraw</p>
           <p className="balance-label">CHOOSE SUM (IN RUPEES)</p>
           <ul className="denominationList">
